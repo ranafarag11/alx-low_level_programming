342a20676017497867bcea17333bc6cdd0f45132
ranafarag11 <ranafarag72@gmail.com>
2023-04-24 13:56:37 -0700

    except y3ni not equal

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ * 
+ * Description: print the alphabet except q & e
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int b = 97;
+
+	while (b < 123)
+	{
+		if (b != 101 && b !=113)
+		{
+			putchar(b);
+		}
+		b++;
+	}
+	putchar(10);
+
+	return (0);
+}
