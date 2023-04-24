6a086a55869eb644b389e42b1b921e6e6f0250ef
ranafarag11 <ranafarag72@gmail.com>
2023-04-24 14:41:23 -0700

    yarab

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers less than 10
+ *
+ * description: print numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	while (a < 10)
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar(10);
+
+	return(0);
+}
