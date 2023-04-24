9a845e0df6d9b4cb5cb75019a7bb23a0b54f672f
ranafarag11 <ranafarag72@gmail.com>
2023-04-24 06:10:49 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,38 +1,22 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
- * main - print the letters of the alphabet
- *
- * Description: print the letters of the alphabet
- *
- * Return: Always 0 (Success)
- */
+* main - function to print the letters of the alphabet
+*
+*Description:used putchar function to  print the letters of the alphabet
+*according to ASCII 
+* Return: 0
+*/
 
 int main(void)
 {
-	int i = 97;
+	int a = 97;
 
-	while (i < 123)
+	while (a < 123)
 	{
-		putchar(i);
-		i++;
+		putchar(a);
+		a++;
 	}
 	putchar(10);
 
-=======
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
->>>>>>> eda808b04f87ff9ffcfc421f6d495605f556438b
-	return (0);
 }
