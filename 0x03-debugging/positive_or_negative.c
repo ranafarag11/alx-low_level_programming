1dba47881ec7bbe661efc67d701028274e51b45a
ranafarag11 <ranafarag72@gmail.com>
2023-04-27 16:49:38 -0700

    5mwah

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void positive_or_negative(int i)
+{
+
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
