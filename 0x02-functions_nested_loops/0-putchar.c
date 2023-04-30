e21108d28325ebb4463c93b01cfbf1536f1a3bb8
ranafarag11 <ranafarag72@gmail.com>
2023-04-30 11:11:32 -0700

    yala bina

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ *  Return: 0 (success)
+ */
+
+int main(void)
+{
+	char t[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(t[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+int _putchar(char c);
+#endif 
